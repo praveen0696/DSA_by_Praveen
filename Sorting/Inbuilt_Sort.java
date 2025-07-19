6ff0cdb3100cb5aae732592f580f3aaae72c221b
Praveen Sharma <sharma04praveen@gmail.com>
2025-07-19 20:51:29 +0530

    Some Sorting technique

--- Sorting/Inbuilt_Sort.java
+++ Sorting/Inbuilt_Sort.java
@@ -1,18 +1,20 @@
 package Sorting;
+
 import java.util.Arrays;
-// import java.util.Collections;
+import java.util.Collections;
 
 public class Inbuilt_Sort {
-    public static void printArr(int arr[]) {
+    public static void printArr(Integer arr[]) {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
     }
 
     public static void main(String[] args) {
-        int arr[] = { 5, 4, 1, 3, 2 };
-        Arrays.sort(arr); // It's execute for Ascending order
-        // Arrays.sort(arr , Collections.reverseOrder()); //It's execute for Decending order
+        Integer arr[] = { 5, 4, 1, 3, 2 };
+        // Arrays.sort(arr); // It's execute for Ascending order
+        Arrays.sort(arr , Collections.reverseOrder()); //It's execute for Decending
+        // order
         printArr(arr);
     }
 }
